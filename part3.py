38003d00771104ae862b5a194b3d1d2df56a7a68
charlek <93940108+sharkycharky@users.noreply.github.com>
2021-12-04 16:45:22 +0000

    A note: for the second comment that says "Part 2," it is meant to refer to "Part 3"

--- part3.py
+++ part3.py
@@ -11,6 +11,6 @@ else: # linked to latest if statement
   print("Negative")
 
 if number % 3 == 0: 
-  print("Divisible by 3")
+  print("Divisible by three")
 else: # linked to latest if statement
-  print("Not divisible by 3")
+  print("Not divisible by three")
